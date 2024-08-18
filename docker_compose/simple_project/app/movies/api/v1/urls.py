5dc2ab8bcddbd828fb8fffd81aa90862a97fe35c
Grigorev Pavel <grigorev@rtkrad.ru>
2024-08-18 18:11:07 +0800

    базовый api

--- docker_compose/simple_project/app/movies/api/v1/urls.py
+++ docker_compose/simple_project/app/movies/api/v1/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework import routers
 
-from movies.api.v1 import views
+from .api import FilmWorkViewSet
 
-urlpatterns = [
-    path('movies/', views.api),
-]
+# from movies.api.v1 import views
+
+# urlpatterns = [
+#     path('movies/', views.api),
+# ]
+router = routers.DefaultRouter()
+router.register(r'filmwork', FilmWorkViewSet)
+urlpatterns = router.urls
